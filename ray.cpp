89e9e5fed57070a92fe24171025847ed306524ec
philip king <144460960+philippkk@users.noreply.github.com>
2024-08-29 20:01:52 -0500

    made it in go

--- ray.cpp
+++ ray.cpp
@@ -3,34 +3,37 @@
 #include "color.h"
 #include "vec3.h"
 
+#define IMAGE_WIDTH 2000
+#define IMAGE_HEIGHT 2000
+
+#define R 0
+#define G 1
+#define B 2
+
 int main()
 {
-    // Image
-    int image_width = 2000;
-    int image_height = 2000;
-    
     FILE * pFile;
     pFile = fopen ("image.ppm", "wb");
 
-    std::fprintf(pFile, "P6\n%d %d\n255\n", image_width, image_height);
+    std::fprintf(pFile, "P6\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    for (int j = 0; j < image_height; j++)
+    for (int j = 0; j < IMAGE_HEIGHT; j++)
     {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        
-        for (int i = 0; i < image_width; i++)
+        std::clog << "\rScanlines remaining: " << (IMAGE_HEIGHT - j) << ' ' << std::flush;
+        for (int i = 0; i < IMAGE_WIDTH; i++)
         {
-            auto pixel_color = color(double(i)/(image_width-1),
-            double(j)/(image_height-1),
+            auto pixel_color = color(double(i)/(IMAGE_WIDTH-1),
+            double(j)/(IMAGE_HEIGHT-1),
             1.0);
+
             auto r = pixel_color.x();
             auto g = pixel_color.y();
             auto b = pixel_color.z();
 
             unsigned char pixel[3];
-            pixel[0] = int(255.999 * r);
-            pixel[1] = int(255.999 * g);
-            pixel[2] = int(255.999 * b);
+            pixel[R] = int(255.999 * r);
+            pixel[G] = int(255.999 * g);
+            pixel[B] = int(255.999 * b);
             
             fwrite (pixel , 1, 3, pFile);
         }
