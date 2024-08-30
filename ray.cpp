6c9d013e159f2d8d698e28b14af31a4b0ade1e0c
philip king <144460960+philippkk@users.noreply.github.com>
2024-08-29 19:25:58 -0500

    now opens automatically on mac and uses better stuff

--- ray.cpp
+++ ray.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+
 int main()
 {
     // Image
-    int image_width = 800;
-    int image_height = 800;
+    int image_width = 2000;
+    int image_height = 2000;
+    
+    FILE * pFile;
+    pFile = fopen ("image.ppm", "wb");
 
-    // Render
-    std::cout << "P3\n"
-              << image_width << ' ' << image_height << "\n255\n";
+    std::fprintf(pFile, "P6\n%d %d\n255\n", image_width, image_height);
 
     for (int j = 0; j < image_height; j++)
     {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        
         for (int i = 0; i < image_width; i++)
         {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 1.0;
-            
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
+            auto pixel_color = color(double(i)/(image_width-1),
+            double(j)/(image_height-1),
+            1.0);
+            auto r = pixel_color.x();
+            auto g = pixel_color.y();
+            auto b = pixel_color.z();
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            unsigned char pixel[3];
+            pixel[0] = int(255.999 * r);
+            pixel[1] = int(255.999 * g);
+            pixel[2] = int(255.999 * b);
+            
+            fwrite (pixel , 1, 3, pFile);
         }
     }
-    std::clog << "\rDone.                 \n";
+
+    
+    fclose (pFile);
+
+    std::clog << "\rDone. opening file, nerd.                 \n";
+
+    std::system("open image.ppm"); 
 }
