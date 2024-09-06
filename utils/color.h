4626f620665d14773f3457a4da656098c93c9dc9
philip king <144460960+philippkk@users.noreply.github.com>
2024-09-06 16:10:22 -0500

    antialias

--- utils/color.h
+++ utils/color.h
@@ -2,7 +2,7 @@
 #define COLOR_H
 
 #include "vec3.h"
-
+#include "interval.h"
 using color = vec3;
 
 void write_color(std::vector<int> &pixel, const color& pixel_color) {
@@ -10,10 +10,11 @@ void write_color(std::vector<int> &pixel, const color& pixel_color) {
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+   // Translate the [0,1] component values to the byte range [0,255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write out the pixel color components.
     pixel.emplace_back(int(255.999 * r));
