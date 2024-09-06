cf628abe6cc52cbedb9d3e901e7526e79172daaf
philip king <144460960+philippkk@users.noreply.github.com>
2024-09-06 15:37:12 -0500

    camera file

--- raycast.cpp
+++ raycast.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 #include "utils/utils.h"
-
+#include "utils/camera.h"
 #include "utils/color.h"
 #include "utils/ray.h"
 #include "utils/vec3.h"
@@ -16,82 +16,18 @@
 #define G 1
 #define B 2
 
-std::vector<int> pixel;
-int image_width,image_height;
-
-color ray_color(const ray& r, const hittable& world) {
-    hit_record rec;
-    if (world.hit(r, interval(0, infinity), rec)) { 
-        return 0.5 * (rec.normal + color(1,1,1));
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
-}
-
-void writeFile(std::vector<int> pixel);
-
 int main()
 {
-    auto aspect_ratio = 16.0/9.0;
-    image_width = IMAGE_WIDTH;
-
-    image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    // World
-
-    hittable_list world;
+   hittable_list world;
 
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
-    // Camera
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
-    auto camera_center = point3(0, 0, 0);
+    camera cam;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = IMAGE_WIDTH;
 
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
-
-    // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-    
-
-    for (int j = 0; j < image_height; j++)
-    {
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < IMAGE_WIDTH; i++)
-        {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color = ray_color(r, world);
-            write_color(pixel,pixel_color); 
-        }
-    }
-
-    writeFile(pixel); 
+    cam.render(world);    
 }
 
-void writeFile(std::vector<int> pixel){
-    FILE* file;
-    file = fopen ("image.ppm", "wb");
-    std::fprintf(file, "P6\n%d %d\n255\n", image_width, image_height);
-    for(int i = 0; i < pixel.size();i++){
-        fwrite(&pixel[i] , 1, 1, file); // write each pixel
-    }
-    fclose (file);
-    std::clog << "\rDone. opening file, nerd.                 \n";
-    std::system("open image.ppm"); 
-}
